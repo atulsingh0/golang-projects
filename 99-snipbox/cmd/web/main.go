bf70c54a837b6082cd81c4369d4f6dc37534fc56
atulsingh0 <3430610+atulsingh0@users.noreply.github.com>
2023-08-04 22:02:04 -0400

    99-snipbox | added arguement

--- 99-snipbox/cmd/web/main.go
+++ 99-snipbox/cmd/web/main.go
@@ -1,24 +1,32 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
 
+	// Define Arguements
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	// Define Server
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", neuter(fileServer)))
 
+	// Register Handlers
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snipView)
 	mux.HandleFunc("/snippet/create", snipCreate)
 
-	log.Println("Starting server on :4000")
+	log.Printf("Starting server on %s\n", *addr)
 
-	if err := http.ListenAndServe(`:4000`, mux); err != nil {
+	// Starting Server
+	if err := http.ListenAndServe(*addr, mux); err != nil {
 		log.Fatal("Unable to start server", err.Error())
 	}
 }
